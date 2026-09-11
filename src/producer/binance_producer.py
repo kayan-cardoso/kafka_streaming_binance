37672546fee929d51d335462dae9f8fbe6a15f09
kayan <kayanestudos7@gmail.com>
2026-09-11 10:27:10 -0300

    ADD: pinot container

--- src/producer/binance_producer.py
+++ src/producer/binance_producer.py
@@ -1,11 +1,20 @@
 import json
+import logging
+from time import sleep
 from datetime import datetime
 from kafka import KafkaProducer
 from kafka.admin import KafkaAdminClient, NewTopic
 
 from src.services.binance_api import get_precos
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+)
+logger = logging.getLogger()
+
 TOPICO = 'cotacoes-binance'
+SEGUNDOS = 30
 admin = KafkaAdminClient(bootstrap_servers='kafka:9092')
 
 try:
@@ -19,19 +28,25 @@ producer = KafkaProducer(
     bootstrap_servers='kafka:9092',
     value_serializer=lambda v: json.dumps(v).encode("utf-8")
 )
+logger.info('Producer conectado com sucesso, iniciando envio das mensagens.')
+while True:
+    try:
+        mensagens = get_precos()
 
+        for mensagem in mensagens:
+            evento = {
+                'cripto':mensagem['symbol'],
+                'price':mensagem['price'],
+                'extract_date':datetime.now().isoformat()
+            }
 
-mensagens = get_precos()
-print(f'foram captadas {len(mensagens)} mensagens.')
+            producer.send(topic=TOPICO, value=evento)
 
-for mensagem in mensagens:
-    evento = {
-        'cripto':mensagem['symbol'],
-        'price':mensagem['price'],
-        'extract_date':datetime.now().isoformat()
-    }
+        producer.flush()
+        logger.info(f'✅ As Mensagens foram enviadas com sucesso.')
+        
+    except Exception as e:
+        logger.warning(f'Erro ao enviar a mensagem: {e}.')
 
-    producer.send(topic=TOPICO, value=evento)
-
-producer.flush()
+    sleep(SEGUNDOS)
 
